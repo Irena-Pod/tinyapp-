6d0ce5351e952886de936e9d27f0820fb5478811
Irena <irena.podgurski@gmail.com>
2020-12-08 01:25:16 +0000

    add GET route to /urls/new

--- express_server.js
+++ express_server.js
@@ -20,7 +20,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
