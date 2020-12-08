298a6de8d019355a2b130445e9f63dee2da7bc2d
Irena <irena.podgurski@gmail.com>
2020-12-08 00:09:08 +0000

    added a second route and template to shortURL

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -18,9 +19,14 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase}
+  const templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
