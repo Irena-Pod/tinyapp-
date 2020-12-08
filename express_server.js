26a00dd54176c29ce93a8e6b2f8c56b4935f1c77
Irena <irena.podgurski@gmail.com>
2020-12-08 17:27:08 +0000

    shortURL:longURL key-value pair saved to database & POST to /urls redirects to /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -4,10 +4,13 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-function generateRandomString() {
-  return Math.random().toString(36).substring(2,8)
-}
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8)
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,7 +18,7 @@ const urlDatabase = {
 };
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -44,10 +47,11 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString()
+  res.redirect(`/urls/:${shortURL}`);
+  urlDatabase[shortURL] = req.body.longURL
+  console.log(urlDatabase)
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+
+
