c89c2b8190347d97b19dc06c1db2f6387a57f003
Irena <irena.podgurski@gmail.com>
2020-12-09 17:10:34 +0000

    implement logout

--- express_server.js
+++ express_server.js
@@ -36,6 +36,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls")
 }) 
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("Username");
+  res.redirect("/urls")
+}) 
+
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase, 
